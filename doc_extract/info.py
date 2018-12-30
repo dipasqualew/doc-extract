b7881c4ab28bc97660262cb0a06c32e8d16955d7
William Di Pasquale <rolafium@protonmail.com>
2018-12-30 18:40:48 +0000

    Project setup

---
+++ doc_extract/info.py
@@ -0,0 +1,18 @@
+"""
+Project Info
+"""
+VERSION = "0.0.1"
+
+PROJECT_TAG = "django_auto_model"
+PROJECT_URL = "https://github.com/rolafium/Django-AutoModel"
+
+AUTHORS = [
+    {
+        "name": "William Di Pasquale",
+        "email": "rolafium@protonmail.com",
+    }
+]
+
+SHORT_DESCRIPTION = "Extracts the docstring from source files and stores them in a tree"
+
+LICENSE_TYPE = "MIT"
---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import find_packages, setup
+from doc_extract.info import (
+    VERSION,
+    PROJECT_TAG,
+    PROJECT_URL,
+    AUTHORS,
+    SHORT_DESCRIPTION,
+    LICENSE_TYPE,
+)
+
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as fp:
+    README = fp.read()
+
+setup(
+    name=PROJECT_TAG,
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    license=LICENSE_TYPE,
+    description=SHORT_DESCRIPTION,
+    long_description=README,
+    url=PROJECT_URL,
+    author=", ".join(author["name"] for author in AUTHORS),
+    author_email=", ".join(author["email"] for author in AUTHORS),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
